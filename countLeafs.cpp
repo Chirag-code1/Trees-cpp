a8699e211eea2deaa9937d5a22d42ccb10152c6f
CHIRAG <78530525+Chirag-code1@users.noreply.github.com>
2021-11-13 11:32:44 +0530

    Create countLeafs.cpp

---
+++ countLeafs.cpp
@@ -0,0 +1,9 @@
+int countLeafs(struct Node* root) 
+{
+    if(root == NULL)
+    return 0;
+    else if(root->left == NULL && root->right == NULL)
+    return 1;
+    else 
+    return countLeafs(root->left) + countLeafs(root->right);
+}
