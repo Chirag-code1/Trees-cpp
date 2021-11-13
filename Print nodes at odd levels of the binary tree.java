4342a0ff7e5ff5648a56a0cdabe474a37189d0f8
CHIRAG <78530525+Chirag-code1@users.noreply.github.com>
2021-11-13 11:32:13 +0530

    Create Print nodes at odd levels of the binary tree.java

---
+++ Print nodes at odd levels of the binary tree.java
@@ -0,0 +1,26 @@
+import java.util.Queue;
+class Result {
+  static void printOdd(Node root) {
+	if (root == null)
+    	    return;
+    	Queue<Node> q = new LinkedList<Node> ();
+    	q.add(root);
+    	boolean isOdd = true;
+    	while (!q.isEmpty())
+    	{
+    		int nodeCount = q.size();
+    		for (int i = 0; i < nodeCount; i++)
+    		{
+    			Node node = q.peek();
+    			q.remove();
+    			if (isOdd)
+    			    System.out.print(node.data + " ");
+    			if (node.leftChild != null)
+    				q.add(node.leftChild);
+              if (node.rightChild != null)
+    				q.add(node.rightChild);
+    		}
+    		isOdd = !isOdd;
+    	}
+  }
+}
